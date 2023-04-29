d6c4c20794dc702a51924ba4aabd5c86cecfcd30
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2023-04-29 16:19:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
-// process.on('uncaughtException', (err) => {
-// 	console.log('UNCAUGHT REJECTION  Shutting down....');
-// 	console.log(err.name, err.message);
-// 	process.exit(1);
-// });
 
 dotenv.config({
 	path: './config.env',
@@ -30,10 +25,3 @@ app.listen(port, () => {
 	console.log(`app running on port ${port}....`);
 });
 
-// process.on('unhandledRejection', (err) => {
-// 	console.log('UNHANDLED REJECTION  Shutting down....');
-// 	console.log(err);
-// 	server.close(() => {
-// 		process.exit(1);
-// 	});
-// });
